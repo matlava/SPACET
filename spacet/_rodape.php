2caf102ef292774387c919d54cf2d0ec5e0930c4
EugenioMatlava <62499791+matlava@users.noreply.github.com>
2023-03-27 05:31:09 +0200

    Add files via upload

---
+++ spacet/_rodape.php
@@ -0,0 +1,7 @@
+    <hr>
+    rodape
+    <script src="inc/js/jquery.slim.min.js"></script>
+    <script src="inc/js/bootstrap.bundle.min.js"></script>
+    <script src="inc/js/main.js"></script>
+</body>
+</html>
---
+++ spacet/index.php
@@ -0,0 +1,9 @@
+<?php
+
+//===============================
+//index
+//==============================
+
+include_once('_cabecalho.php');
+echo '<p>Teste</p>';
+include_once('_rodape.php');
